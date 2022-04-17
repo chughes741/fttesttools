de390a803054417131e315afac1db0229bbf9ec9
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-17 14:58:33 -0400

    Refactoring test functions

--- fttesttools.c
+++ fttesttools.c
@@ -1,68 +1,27 @@
 #include "fttesttools.h"
 
 /*    Test tools       */
-void int_expect_eq(char *test_name, int real_val, int test_val) {
-	if (real_val == test_val) {
-		set_green();
+
+
+int *expect_eq(char *test_name, void *real_val, void *test_val) {
+	if ((int *)real_val == (int *)test_val) {
+		printf("%s", green);
 		printf("%s passed\n", test_name);
-		return;
-		}
-	set_red();
-	printf("%s failled\n", test_name);
-	return;
+		return 0;
+	}
+	printf("%s", red);
+	printf("%s failed\n", test_name);
+	printf("Expected: %s, \nResult: %s\n", real_val, test_val);
+	return 1;	
 }
 
-void int_expect_neq(char *test_name, int real_val, int test_val) {
+int expect_neq(char *test_name, void *real_val, void *test_val) {
 	if (real_val != test_val) {
-		set_green();
+		printf("%s", green);
 		printf("%s passed\n", test_name);
-		return;
-		}
-	set_red();
-	printf("%s failled\n", test_name);
-	return;
-}
-
-void char_expect_eq(char *test_name, char real_val, char test_val) {
-	if (real_val == test_val) {
-		set_green();
-		printf("%s passed\n", test_name);
-		return;
-		}
-	set_red();
-	printf("%s failled\n", test_name);
-	return;
-}
-
-void char_expect_neq(char *test_name, char real_val, char test_val) {
-	if (real_val != test_val) {
-		set_green();
-		printf("%s passed\n", test_name);
-		return;
-		}
-	set_red();
-	printf("%s failled\n", test_name);
-	return;
-}
-
-void sizet_expect_eq(char *test_name, size_t real_val, size_t test_val) {
-	if (real_val == test_val) {
-		set_green();
-		printf("%s passed\n", test_name);
-		return;
-		}
-	set_red();
-	printf("%s failled\n", test_name);
-	return;
-}
-
-void sizet_expect_neq(char *test_name, size_t real_val, size_t test_val) {
-	if (real_val != test_val) {
-		set_green();
-		printf("%s passed\n", test_name);
-		return;
-		}
-	set_red();
-	printf("%s failled\n", test_name);
-	return;
+		return 0;
+	}
+	printf("%s", red);
+	printf("%s failed\n", test_name);
+	return 1;
 }
