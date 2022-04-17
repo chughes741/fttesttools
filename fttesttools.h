fa626c6a07ab2547b2ae45dde6c4b3fe6001edee
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-17 14:59:05 -0400

    Changed the color defines

--- fttesttools.h
+++ fttesttools.h
@@ -8,13 +8,17 @@
 
 /* Print to set output color */
 #define default "\033[0m"
-#define black "\033[0;30"
-#define red "\033[0;31"
-#define green "\033[0;32"
-#define yellow "\033[0;33"
-#define blue "\033[0;34"
-#define purple "\033[0;35"
-#define cyan "\033[0;36"
-#define white "\033[0;37"
+#define black "\033[1;30m"
+#define red "\033[1;31m"
+#define green "\033[1;32m"
+#define yellow "\033[1;33m"
+#define blue "\033[1;34m"
+#define purple "\033[1;35m"
+#define cyan "\033[1;36m"
+#define white "\033[1;37m"
+
+/* Testing functions */
+int expect_eq(char *test_name, void *real_val, void *test_val);
+int expect_neq(char *test_name, void *real_val, void *test_val);
 
 #endif
