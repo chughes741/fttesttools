99663b8bc268ce944e1a9d4b1e0bf1083013cfef
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-17 12:50:14 -0400

    Created a header for the tools

---
+++ fttesttools.h
@@ -0,0 +1,20 @@
+
+#ifndef FT_TEST_TOOLS
+#define FT_TEST_TOOLS
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/* Print to set output color */
+#define default "\033[0m"
+#define black "\033[0;30"
+#define red "\033[0;31"
+#define green "\033[0;32"
+#define yellow "\033[0;33"
+#define blue "\033[0;34"
+#define purple "\033[0;35"
+#define cyan "\033[0;36"
+#define white "\033[0;37"
+
+#endif
